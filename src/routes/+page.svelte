9e206adf2844fad7951dd30581534c61217631b6
oggnimodd <64012087+oggnimodd@users.noreply.github.com>
2025-02-28 17:58:52 +0700

    fix: sampler release value

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -298,6 +298,7 @@
           "F#4": "Fs4.mp3",
           A4: "A4.mp3",
         },
+        release: 1,
         baseUrl: "https://tonejs.github.io/audio/salamander/",
       }).toDestination();
     }
